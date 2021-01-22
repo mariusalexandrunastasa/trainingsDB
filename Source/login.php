bf05a6dc32e41c3a2232ded2c7b71d4f632c0371
mariusalexandrunastasa <mariusalexandrunastasa@gmail.com>
2021-01-22 19:47:02 +0200

    last of last

--- Source/login.php
+++ Source/login.php
@@ -12,15 +12,6 @@
     $activePage = $_SERVER['REQUEST_URI'];
     ?>
 
-    <div class="header">
-        <a href="/index.php" class="logo">Training Management</a>
-        <div class="header-right">
-            <a class="<?= ($activePage == '/index.php' || $activePage == '/') ? 'active' : ''; ?>" href="/index.php">Home</a>
-            <a class="<?= ($activePage == '/trainings.php') ? 'active' : ''; ?>" href="/trainings.php">Trainings</a>
-            <a class="<?= ($activePage == '/analytics.php') ? 'active' : ''; ?>" href="/analytics.php">Analytics</a>
-        </div>
-    </div>
-
     <form action="db/login.php" method="post">
 
         <div class="container">
